ab3ce249597bc9de5aae48a2f384ff8c6d6b5249
Miguel Padilla <padilla_mg60@yahoo.com>
2023-02-01 15:18:27 -0600

    Stopwatch project start

---
+++ src/StopWatch.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class StopWatch implements ActionListener{
+
+	StopWatch(){
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
